c0c3cda57c63384de4eceb770edc1887a7cebe75
Michael Lazebny <michael@lazebny.io>
2024-05-03 09:16:41 +0200

    added country picker

--- lib/menu_anchor.dart
+++ lib/menu_anchor.dart
@@ -1,6 +1,8 @@
 import 'dart:math';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:popup_playground/countries.dart';
 
 class MenuAnchorShowcase extends StatefulWidget {
   const MenuAnchorShowcase({super.key});
@@ -10,8 +12,9 @@ class MenuAnchorShowcase extends StatefulWidget {
 }
 
 class _MenuAnchorShowcaseState extends State<MenuAnchorShowcase> {
-  late final MenuController _styledController;
-  late final MenuController _customController;
+  final MenuController _styledController = MenuController();
+  final MenuController _customController = MenuController();
+  final MenuController _countryController = MenuController();
 
   void _onItemPressed(String item) {
     ScaffoldMessenger.of(context)
@@ -20,13 +23,6 @@ class _MenuAnchorShowcaseState extends State<MenuAnchorShowcase> {
         content: Text('Item $item pressed'),
         duration: const Duration(seconds: 1),
       ));
-  }
-
-  @override
-  void initState() {
-    _styledController = MenuController();
-    _customController = MenuController();
-    super.initState();
   }
 
   @override
@@ -124,11 +120,120 @@ class _MenuAnchorShowcaseState extends State<MenuAnchorShowcase> {
                 label: const Text('Show Custom Menu'),
               ),
             ),
+            MenuAnchor(
+              controller: _countryController,
+              style: MenuStyle(
+                shape: MaterialStatePropertyAll(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+              ),
+              menuChildren: [
+                _CountryMenu(controller: _countryController),
+              ],
+              builder: (context, controller, child) => FilledButton.icon(
+                onPressed: controller.open,
+                icon: const Icon(Icons.flag_outlined),
+                label: const Text('Show Country Picker'),
+              ),
+            ),
           ],
         ),
       ],
     );
   }
+}
+
+class _CountryMenu extends StatefulWidget {
+  const _CountryMenu({required this.controller});
+
+  final MenuController controller;
+
+  @override
+  State<_CountryMenu> createState() => _CountryMenuState();
+}
+
+class _CountryMenuState extends State<_CountryMenu> {
+  final _filter = TextEditingController();
+
+  @override
+  void dispose() {
+    _filter.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => MouseRegion(
+        cursor: SystemMouseCursors.basic,
+        child: SizedBox(
+          width: min(MediaQuery.sizeOf(context).width - 50, 200),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Menu Header',
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.cancel),
+                      onPressed: widget.controller.close,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: TextField(
+                  controller: _filter,
+                  decoration: const InputDecoration(
+                    filled: true,
+                    isDense: true,
+                    border: OutlineInputBorder(),
+                    hintText: 'Start typing country..',
+                  ),
+                ),
+              ),
+              const SizedBox(height: 8),
+              ValueListenableBuilder(
+                valueListenable: _filter,
+                builder: (context, value, _) {
+                  final countries = countryList.where(
+                    (country) => country.name.toLowerCase().contains(value.text.toLowerCase()),
+                  );
+                  return SizedBox(
+                    height: min(countries.length * 48.0, 200),
+                    child: ListView.builder(
+                      clipBehavior: Clip.antiAlias,
+                      itemCount: countries.length,
+                      itemBuilder: (context, index) => ListTile(
+                        title: Text(countries.elementAt(index).name),
+                        onTap: () {
+                          widget.controller.close();
+                          ScaffoldMessenger.of(context)
+                            ..clearSnackBars()
+                            ..showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                  'Country selected: ${countries.elementAt(index).name}',
+                                ),
+                                duration: const Duration(seconds: 1),
+                              ),
+                            );
+                        },
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      );
 }
 
 class _CustomMenu extends StatefulWidget {
